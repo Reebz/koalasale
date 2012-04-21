079acb285b649c2f47383ddf9eb4eb1963a2ef4b
Reebz <mitchell.ribar@gmail.com>
2012-04-21 19:56:19 -0400

    Initial commit v2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koalasales::Application.config.secret_token = 'bca58f8b2dd50939e259a898f5c8ded46ebb25cddbf32d02ef229f3ce69159740576ca425a148c03a419429eef389581b3f4e92970a7a9485f325ed28413bcdf'
